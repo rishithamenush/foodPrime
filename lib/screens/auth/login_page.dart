102f4c13a0d6d71cc4a9f6398f7335e2b8045eb5
rishitha.menush <rishitha.menush@gmail.com>
2024-08-09 13:19:25 +0530

    Add the Authentication Screens

--- lib/screens/auth/login_page.dart
+++ lib/screens/auth/login_page.dart
@@ -1,14 +1,35 @@
 
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+import '../../widgets/form_container_widget.dart';
+
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Login Page"),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+        child: Column(
+          children: [
+            const SizedBox(height: 140,),
+            Text("Log In", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+             const SizedBox(height: 140,),
+            FormContainerWidget(hintText:"Email or UserName"),
+            const SizedBox(height: 20,),
+            FormContainerWidget(hintText: "Password"),
+            const SizedBox(height: 20,)
+          ],
+        ),
       ),
     );
   }
