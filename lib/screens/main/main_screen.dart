a73b710809b89f79179a99a9b364d91d1f17cb55
rishitha.menusha <rishitha.menush@gmail.com>
2024-08-12 15:28:48 +0530

    Update the Main Screen

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:foodprime/theme/style.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentPageIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Main Screen"),),
+    return  Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (index){
+            setState(() {
+              _currentPageIndex = index;
+            });
+          },
+          currentIndex: _currentPageIndex,
+          selectedItemColor: primaryColorED6E1B,
+          selectedLabelStyle: const TextStyle(color: primaryColorED6E1B),
+          items:const [
+            BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.shopping_cart),label: "Cart"),
+            BottomNavigationBarItem(icon: Icon(Icons.account_circle_rounded), label: "Account")
+          ]
+      ),
+      body: _switchPagesOnIndex(_currentPageIndex);
     );
+  }
+  _switchPagesOnIndex(int index){
+    switch(index){
+      case 0: {
+        return HomePage();
+      }
+      case 1: {
+        return CartPage();
+      }
+      case 2: {
+        return ProfilePage();
+      }
+    }
   }
 }
