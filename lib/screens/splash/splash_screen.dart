d2140bd1d5717dc1288706262d8d0fcd0e4fd158
rishitha.menusha <rishitha.menush@gmail.com>
2024-08-12 09:19:57 +0530

    Add the animation effect to the splash logo

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -11,20 +11,49 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
   @override
   void initState() {
-     Future.delayed(const Duration(milliseconds: 3000)).then((value){
-       Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => widget.child ), (route)=> false);
-     });
     super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    );
+
+    _animation = Tween(begin: 2.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+
+    _animationController.forward();
+
+    Future.delayed(const Duration(milliseconds: 3000)).then((value) {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => widget.child),
+            (route) => false,
+      );
+    });
   }
 
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primaryColorED6E1B,
       body: Center(
-        child: Image.asset("assets/app_logo.png"),
+        child: ScaleTransition(
+          scale: _animation,
+          child: Image.asset("assets/app_logo.png"),
+        ),
       ),
     );
   }
