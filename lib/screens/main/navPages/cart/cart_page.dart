e17ea85926163dd7f1ba52803ad7313bebd5fcf0
rishitha.menusha <rishitha.menush@gmail.com>
2024-08-13 07:48:39 +0530

    Update the Cart page

--- lib/screens/main/navPages/cart/cart_page.dart
+++ lib/screens/main/navPages/cart/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -10,6 +11,8 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+    );
   }
 }
