aa506bc042c3af24f5f614d9b108568e23a2f24c
rishitha.menusha <rishitha.menush@gmail.com>
2024-08-14 11:38:14 +0530

    Add the appBar for the profile_page.dart

--- lib/screens/main/navPages/profile/profile_page.dart
+++ lib/screens/main/navPages/profile/profile_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../theme/style.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -10,6 +13,25 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: whiteColor,
+        centerTitle: true,
+        title: Image.asset(
+          "assets/word_app_logo.png",
+          fit: BoxFit.contain,
+          width: 150,
+          height: 50,
+        ),
+        actions: [
+          Transform.translate(offset: const Offset(0, -5),
+          child: IconButton(
+            onPressed: (){},
+            icon: const Icon(Icons.settings),
+          ),
+          ),
+        ],
+      ),
+    );
   }
 }
