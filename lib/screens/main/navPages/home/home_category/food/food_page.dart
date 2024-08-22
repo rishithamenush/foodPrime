55a9771da13a442ec35ab5da086741c894503cab
rishitha.menusha <rishitha.menush@gmail.com>
2024-08-22 16:30:10 +0530

    Update the food page

--- lib/screens/main/navPages/home/home_category/food/food_page.dart
+++ lib/screens/main/navPages/home/home_category/food/food_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +15,7 @@ class FoodPage extends StatefulWidget {
 }
 
 class _FoodPageState extends State<FoodPage> {
+  int _currentSelectedCategory = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,9 +79,119 @@ class _FoodPageState extends State<FoodPage> {
           const SizedBox(
             height: 20,
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _singleCategoryItemWidget(
+                  title: "Fruits",
+                  color: _currentSelectedCategory == 0
+                      ? primaryColorED6E1B
+                      : Colors.grey[400] ?? Colors.grey,
+                  onTap: (){
+                    setState((){
+                      _currentSelectedCategory = 0;
+                    });
+                  }
+              ),
+              _singleCategoryItemWidget(
+                  title: "Vegitables",
+                  color: _currentSelectedCategory == 1
+                      ? primaryColorED6E1B
+                      : Colors.grey[400] ?? Colors.grey,
+                  onTap: (){
+                    setState((){
+                      _currentSelectedCategory = 1;
+                    });
+                  }
+              ),
+              _singleCategoryItemWidget(
+                  title: "Kitchen",
+                  color: _currentSelectedCategory == 2
+                      ? primaryColorED6E1B
+                      : Colors.grey[400] ?? Colors.grey,
+                  onTap: (){
+                    setState((){
+                      _currentSelectedCategory = 2;
+                    });
+                  }
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          const Text("Today Special Offer", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+          const SizedBox(
+            height: 20,
+          ),
+          _switchSpecialCategoryOnSelectedIndex(_currentSelectedCategory),
+
+          const Text("Today Popular Now", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+          const SizedBox(
+            height: 20,
+          ),
+          _switchPopularCategoryOnSelectedIndex(_currentSelectedCategory),
+
         ],
       ),
     ),
     );
+  }
+  _singleCategoryItemWidget({required VoidCallback onTap, required Color color, required String title}){
+    return Expanded(
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 5),
+          width: 110,
+          height: 40,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20), color: color,
+          ),
+          child: Center(
+            child: Text(
+              "$title",
+              style: const TextStyle(color: whiteColor),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  //Switch Special List
+  _switchSpecialCategoryOnSelectedIndex(int index){
+    switch(index){
+      case 0:
+        {
+          return _buildSpecialBurgerList();
+        }
+      case 1:
+        {
+          return _buildSpecialPizzaList();
+        }
+      case 2:
+        {
+          return _buildSpecialSandwichList();
+        }
+    }
+  }
+
+  //Switch Popular List
+  _switchPopularCategoryOnSelectedIndex(int index){
+    switch(index){
+      case 0:
+        {
+          return _buildPopularBurgerList();
+        }
+      case 1:
+        {
+          return _buildPopularPizzaList();
+        }
+      case 2:
+        {
+          return _buildPopularSandwichList();
+        }
+    }
   }
 }
