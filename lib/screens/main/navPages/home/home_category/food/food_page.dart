efebc76e5fff09450c80a235133b71ae4a5c0158
rishitha.menusha <rishitha.menush@gmail.com>
2024-08-22 16:18:16 +0530

    Add the search bar for the food page

--- lib/screens/main/navPages/home/home_category/food/food_page.dart
+++ lib/screens/main/navPages/home/home_category/food/food_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../../theme/style.dart';
+import '../../../../../search/search_page.dart';
+import '../../../../../search/search_widget.dart';
 
 class FoodPage extends StatefulWidget {
   const FoodPage({super.key});
@@ -35,6 +37,48 @@ class _FoodPageState extends State<FoodPage> {
           height: 50,
         ),
       ),
+
+    body: Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: SearchWidget(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => SearchPage() ));
+                    }
+                ),
+              ),
+              const SizedBox(width: 10,),
+              Container(
+                width: 40,
+                height: 40,
+                decoration: const BoxDecoration(
+                  color: primaryColorED6E1B,
+                  shape: BoxShape.circle,
+                ),
+                child: const Center(
+                  child: Icon(
+                    Icons.settings_voice,
+                    color: whiteColor,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Text("Categories", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    ),
     );
   }
 }
