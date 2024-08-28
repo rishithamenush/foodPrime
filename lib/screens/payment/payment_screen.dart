40057e8ee446e754eea435999c4f8a5621f1df48
rishitha.menusha <rishitha.menush@gmail.com>
2024-08-28 11:45:08 +0530

    Add the payment screen

--- lib/screens/payment/payment_screen.dart
+++ lib/screens/payment/payment_screen.dart
@@ -1,6 +1,7 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../theme/style.dart';
 
 class PaymentScreen extends StatelessWidget {
   const PaymentScreen({super.key});
@@ -8,7 +9,26 @@ class PaymentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Payment Screen"),),
+      appBar: AppBar(
+        backgroundColor: whiteColor,
+        centerTitle: true,
+        title: const Text(
+          "Payment",
+          style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold, fontSize: 22 ), // Added text color to ensure visibility
+        ),
+      ),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Select Payment Method",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+
+      )
     );
   }
 }
